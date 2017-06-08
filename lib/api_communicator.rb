0f803136003756ae6ad644eb2ba7c36f4a427903
wbdana <william.b.dana@gmail.com>
2017-06-08 09:30:48 -0400

    testing adding additional pages numbers in comments

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,24 +2,44 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character)
-  #make the web request
+
+## Adding pages from API to #character_hash
+#  1. all_characters = RestClient.get('http://swapi.co/api/people')
+#  2. character_hash = JSON.parse(all_characters)
+#  3. if character_hash["next"].include?("page=")
+#       all_characters << RestClient.get('http://swapi.co/api/people/?page=INCREMENTAL VARIABLE COUNTER')
+#       character_hash = JSON.parse(all_characters)
+
+
+
+def character_hash
+  # requests data for all characters from Star Wars API and parses it into JSON
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+  # count = 2
+  #
+  # if character_hash["next"].include?("page=")
+  #   all_characters << RestClient.get(character_hash["next"])
+  #   character_hash = JSON.parse(all_characters)
+  #   count += 1
+  # end
+end
+
+def get_film_urls(character)
+  # if the character name exists in the character hash, returns an array containing the Star Wars API URL for each film the character appears in
+  character_hash["results"].find{|character_data| character_data["name"].downcase == character}["films"]
+end
+
+def get_character_movies_from_api(character)
+  # character_hash["results"] is an array of hashes, where each hash represents a character
+  # name is character_hash["results"][<index>]["name"], though we'd likely "each" or "select"
+  # list of film API urls for a given character is character_hash["results"][<index>]["films"]
+  get_film_urls(character).map {|url| JSON.parse(RestClient.get(url))}
 end
 
 def parse_character_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  films_hash.each_with_index{|film, index| puts "#{index + 1} #{film["title"]}"}
 end
 
 def show_character_movies(character)
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,10 @@
 def welcome
   # puts out a welcome message here!
+  puts "Welcome to the Star Wars character directory! We can tell you all films any character appears in."
 end
 
 def get_character_from_user
   puts "please enter a character"
   # use gets to capture the user's input. This method should return that input, downcased.
+  gets.chomp.downcase
 end

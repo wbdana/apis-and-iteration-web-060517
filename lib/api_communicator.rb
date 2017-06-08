767e288b235e962033f88d0dc4d99069bc41bd4c
wbdana <william.b.dana@gmail.com>
2017-06-08 09:49:05 -0400

    updates

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -13,7 +13,6 @@ require 'pry'
 
 
 def character_hash
-  # requests data for all characters from Star Wars API and parses it into JSON
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
   # count = 2
@@ -26,19 +25,14 @@ def character_hash
 end
 
 def get_film_urls(character)
-  # if the character name exists in the character hash, returns an array containing the Star Wars API URL for each film the character appears in
   character_hash["results"].find{|character_data| character_data["name"].downcase == character}["films"]
 end
 
 def get_character_movies_from_api(character)
-  # character_hash["results"] is an array of hashes, where each hash represents a character
-  # name is character_hash["results"][<index>]["name"], though we'd likely "each" or "select"
-  # list of film API urls for a given character is character_hash["results"][<index>]["films"]
   get_film_urls(character).map {|url| JSON.parse(RestClient.get(url))}
 end
 
 def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
   films_hash.each_with_index{|film, index| puts "#{index + 1} #{film["title"]}"}
 end
 
